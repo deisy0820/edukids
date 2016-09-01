d3dca147ce2963cc83154efec20707632091b576
deisymsqr6@gmail.com <deisymsqr6@gmail.com>
2016-09-01 11:43:08 -0500

    cambio en la aplicacion.rb precompil

---
+++ public/assets/script-de09b4fc9a6626fb7f9c5bdf1c7495f46a6ef7809dde61f8ffe28c9ec689ed3a.js
@@ -0,0 +1,78 @@
+
+
+    $(document).ready( function(){
+
+      var morado=false,azul=false,rojo=false,amarillo=false;
+        
+
+
+         $('.verde').mouseup( function(){
+             
+              $('audio')[0].play();
+              verde=true;
+              
+          })
+
+         $('.rojo').mouseup( function(){
+             
+              $('audio')[1].play();
+              rojo=true;
+              
+          })
+          $('.amarillo').mouseup( function(){
+             
+              $('audio')[2].play();
+              amarillo=true;
+              
+          })
+
+          $('.azul').mouseup( function(){
+             
+              $('audio')[3].play();
+              azul=true;
+              
+          })
+
+         $('.morado').mouseup( function(){
+             
+              $('audio')[4].play();
+               morado=true;
+    
+          })
+
+          $('.naranja').mouseup( function(){
+             
+              $('audio')[9].play();
+              naranja=true;
+              
+          })
+          $('.cafe').mouseup( function(){
+             
+              $('audio')[6].play();
+              cafe=true;
+              
+          })
+          $('.rosa').mouseup( function(){
+             
+              $('audio')[7].play();
+              rosa=true;
+              
+          })
+          $('.blanco').mouseup( function(){
+             
+              $('audio')[8].play();
+              blanco=true;
+              
+          })
+          $('.rosado').mousemove( function(){
+             
+              $('audio')[5].play();
+              rosado=true;
+              
+          })
+
+
+
+   })
+ 
+;
---
+++ public/assets/script2-d652f34dc2e281889d8617825799d33277d25845af739277b1cefd5964c98cdc.js
@@ -0,0 +1,98 @@
+document).ready( function(){
+
+      var morado=false,azul=false,rojo=false,amarillo=false;
+        
+
+
+         $('.verde').mouseup( function(){
+             
+              $('audio')[0].play();
+              verde=true;
+              
+          })
+
+          $('.azul').mouseup( function(){
+             
+              $('audio')[1].play();
+              azul=true;
+           })
+              $('.amarillo').mouseup( function(){
+             
+              $('audio')[2].play();
+              amarillo=true;
+              
+          })
+
+
+          $('.morado').mouseup( function(){
+             
+              $('audio')[4].play();
+               morado=true;
+
+
+                 })
+    
+         /*$('.verde2').mouseup( function(){
+             
+              $('audio')[1].play();
+              verde=true;
+              
+         //  })
+
+                
+              
+          })
+
+
+        
+        
+
+          $('.naranja').mouseup( function(){
+             
+              $('audio')[5].play();
+              narajado=true;
+              
+          })
+          $('.cafe').mouseup( function(){
+             
+              $('audio')[6].play();
+              cafe=true;
+              
+          })
+          $('.cafe2').mouseup( function(){
+             
+              $('audio')[7].play();
+              cafe=true;
+              
+          })
+          $('.rosa').mouseup( function(){
+             
+              $('audio')[8].play();
+              rosa=true;
+              
+          })
+          $('.blanco').mouseup( function(){
+             
+              $('audio')[9].play();
+              blanco=true;
+              
+          })
+          $('.negro').mousemove( function(){
+             
+              $('audio')[10].play();
+              rosado=true;
+              
+          })
+          $('.negro2').mousemove( function(){
+             
+              $('audio')[11].play();
+              rosado=true;
+              
+          }) */
+
+
+
+
+   })
+ 
+;
